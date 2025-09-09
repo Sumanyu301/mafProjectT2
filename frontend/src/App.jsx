fbc5fc20d2efc45b4167c497b0e687c6e65ac3ed
Hemangi <anvi1324.be22@chitkara.edu.in>
2025-09-09 11:19:06 +0530

    Frontend for admin - create projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import AdminProjects from "./pages/AdminProjects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         {/* later: <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/admin" element={<AdminProjects />} />
       </Routes>
     </Router>
   );
